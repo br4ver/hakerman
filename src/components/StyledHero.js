753d24954e9f7bff409047aa1a76f01b01335fd2
br4ver <qwertynorbz11@gmail.com>
2019-11-12 09:02:44 +0800

    updated hero view hieght

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
-import BackgroundImage from 'gatsby-background-image'
+import React from "react"
+import styled from "styled-components"
+import BackgroundImage from "gatsby-background-image"
 import { useStaticQuery, graphql } from "gatsby"
 
 const getImage = graphql`
   query {
     defaultBcg: file(relativePath: { eq: "defaultBcg.webp" }) {
       childImageSharp {
-        fluid(quality: 90, maxWidth: 4160) {
+        fluid(quality: 100, maxWidth: 4160) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
@@ -15,24 +15,27 @@ const getImage = graphql`
   }
 `
 
-const StyledHero = ({img, className, children, home}) => {
-const data = useStaticQuery(getImage)
-    return (
-        <BackgroundImage className={className} fluid={img||data.defaultBcg.childImageSharp.fluid} home={home}>
-            {children}
-        </BackgroundImage>
-    )
+const StyledHero = ({ img, className, children, home }) => {
+  const data = useStaticQuery(getImage)
+  return (
+    <BackgroundImage
+      className={className}
+      fluid={img || data.defaultBcg.childImageSharp.fluid}
+      home={home}
+    >
+      {children}
+    </BackgroundImage>
+  )
 }
 
 export default styled(StyledHero)`
-  min-height: ${props => (props.home ? "calc(100vh - 62px)" : "50vh")};
+  min-height: ${props => (props.home ? "95vh" : "50vh")};
   background-position: center;
   background-size: cover;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   position: fixed;
-  z-index: -10;
   opacity: 1 !important;
   display: flex;
   justify-content: flex-start;
