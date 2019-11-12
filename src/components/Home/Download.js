73866a686c0dda30e191147cb28850158f9175bc
br4ver <qwertynorbz11@gmail.com>
2019-11-12 14:13:38 +0800

    add polyfill support

--- src/components/Home/Download.js
+++ src/components/Home/Download.js
@@ -2,7 +2,6 @@ import React from "react"
 import styles from "../../css/about.module.css"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import {Link} from 'gatsby'
 const getAbout = graphql`
   query aboutImage {
     aboutImage: file(relativePath: { eq: "goferphone.png" }) {
@@ -21,47 +20,43 @@ const Download = () => {
     <section id="home-download" className={styles.about}>
       <div className={styles.aboutCenter}>
         <div>
-                  <article
-          data-aos="fade-right"
-          data-aos-easing="ease-in-sine"
-          data-aos-once="true"
-          className={styles.aboutImg}
-        >
-          <h4
-            className={styles.downLoad}
-            data-aos="fade-down"
+          <article
+            data-aos="fade-right"
             data-aos-easing="ease-in-sine"
             data-aos-once="true"
-            data-aos-anchor="#trigger-download"
-            data-aos-duration="1500"
+            className={styles.aboutImg}
           >
-            DOWNLOAD
-          </h4>
-          <div className={styles.imgContainer}>
-            <Img fluid={aboutImage.childImageSharp.fluid} alt="about image" />
-          </div>
-        </article>
+            <h4
+              className={styles.downLoad}
+              data-aos="fade-down"
+              data-aos-easing="ease-in-sine"
+              data-aos-once="true"
+              data-aos-anchor="#trigger-download"
+              data-aos-duration="1500"
+            >
+              DOWNLOAD
+            </h4>
+            <div className={styles.imgContainer}>
+              <Img fluid={aboutImage.childImageSharp.fluid} alt="about image" />
+            </div>
+          </article>
         </div>
         <div>
-        <article
-          id="trigger-download"
-          data-aos="fade-down"
-          data-aos-once="true"
-          data-aos-easing="linear"
-          data-aos-duration="500"
-          className={styles.aboutInfo}
-        >
-          <h2>Download Now</h2>
+          <article
+            id="trigger-download"
+            data-aos="fade-down"
+            data-aos-once="true"
+            data-aos-easing="linear"
+            data-aos-duration="500"
+            className={styles.aboutInfo}
+          >
+            <h2>Download Now</h2>
 
-          
-            <Link className="btn-primary">
-          Download
-          </Link>
-            
-        
-        </article>
+            <button type="button" className="btn-primary">
+              <a href="https://google.com">Download</a>
+            </button>
+          </article>
         </div>
-
       </div>
     </section>
   )
